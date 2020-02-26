892c6e6311ce49c1a43676072536103c668fae29
bhawanab77 <bhawanab77@gmail.com>
2020-02-26 15:37:25 +0530

    new changes

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res) => {
     res.render('index',{
         title: 'Weather App',
-        name: 'Andrew Mead'
+        name: 'Bhawana Bisht'
     });
 })
 app.get('/about',(req,res) => {
